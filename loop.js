e49b52f0aea2beabb8d22a2a3880601022c7e550
Sachin Saini <sachin.saini@ap2v.com>
2021-04-29 19:34:17 +0530

    javascript loop

---
+++ loop.js
@@ -0,0 +1,99 @@
+// Loop
+// 
+// while  -> it will check -> 
+// do while -> one 
+// for  ---> (ini, condi, counter)
+
+// while (condition){
+//     loop body
+//     loop body
+//     loop body
+// }
+// condition --> true or false
+
+// let a = 10;
+
+// // //      15 < 15
+// while (a < 15) {
+//     document.write(a + "<br>");
+//     document.write("------------<br>");
+//     a++
+// }
+
+// document.write("EOF");
+
+// do{
+//     body of loop;
+//     body of loop;
+// }while (condition)
+
+// let a = 10
+
+// do {
+//     document.write(a + "<br>");
+//     a++
+// } while (a < 5) // false
+
+// for (initialzation, condition, increament){
+//     body of loop;
+//     body of loop;
+//     body of loop;
+// }
+// ini --> 
+// condi --> true or false
+
+// for --> of , in
+
+
+// for (let a = 10; a <= 20; a++) {
+//     document.write(a, "<br>")
+// }
+
+// logical 
+
+// let a = 10
+// let b = 20
+
+// while ((a < 20) && (b > 10)) {
+//     document.write(a + ", ");
+//     a++
+//     document.write(b + "<br>");
+//     b--
+// }
+
+// Nested Loop
+// let i, j;
+
+// i = 2
+// for (i = 1; i < 5; i++) {
+//     for (j = 1; j < 5; j++) {
+//         document.write("Inside Loop: " + j + "<br>");
+//     }
+//     document.write("Outside Loop: " + i + "<br>");
+// }
+// function(sachin + eddy)
+
+// Loop controller
+// break, 
+// continue
+// switch --> 
+
+// let a = 10; // a = 10
+// //     10 <=15 -> true
+// //     11 <=15 -> true
+// //     12 <= 15 -> true
+// //     13 <= 15 _. true
+// //     14 
+// while (a <= 15) {   
+//     ++a
+//     // 11 == 13 -> false
+//     // 12 == 13  -> false
+//     // 13 == 13  -. true
+//     if (a == 13) {
+//         continue
+//     }
+//     document.write(a + "<br>");
+// }
+
+// https://quizizz.com/join?gc=417830
+// -->
