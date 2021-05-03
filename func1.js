0f24f89c9faa6c4cfd38e9b8bcc0c49d99b6b4b6
Sachin Saini <sachin.saini@ap2v.com>
2021-05-03 19:51:55 +0530

    function and numbers

---
+++ func1.js
@@ -0,0 +1,182 @@
+// Funtions ---> 
+// Functions ?
+// Inbuilt , User defined Functions ? 
+
+
+// alert("Hi all!")
+
+// keyword ---> function keyword
+// let , var
+
+// function function_name(param1, param2){
+//     statement1
+//     statement2
+//     statement3
+// }
+
+// Invoke , call 
+
+// function_name()
+
+// function hello(first_name, last_name) {
+//     document.write("Starting of func..<br>");
+//     document.write("Hey " + first_name + " " + last_name + " Good Evening!" + "<br>");
+//     document.write("Exiting...<br>");
+// }
+
+
+// hello("Adesh", "Mantur");
+
+// hello("Sachin!", "Saini");
+
+
+// hello(300, 20);
+// 
+
+
+// function add(num1, num2, num3 = 0) {
+//     let sum = num1 + num2 + num3;
+//     document.write('num1 ' + num1 + "<br>");
+//     document.write('num2 ' + num2 + "<br>");
+//     document.write('num3 ' + num3 + "<br>");
+//     document.write(sum);
+// }
+
+// add(2, 5, "sachin");
+
+// function skillsanta(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// skillsanta('sachin', 100, 2.5);
+
+
+// add(100, 200, 400);
+
+
+// function hello() {
+//     var a = 10;
+//     console.log(a);
+// }
+
+// hello();
+// console.log(a)
+
+// -----------> 
+
+// Number ---> random
+
+// 12.5    -----> number
+// 12     ----> number
+
+// 12.4
+
+
+
+// var num1 = 42.251231223334;
+
+// num1 = num1.toFixed(2);
+
+// document.write(num1);
+
+// rounding ---> 
+// floor --> rounds down
+// ceil  --> rounds up
+// round --> 
+// 0-4 << down d
+// 5-9 --> round up 
+// var a = 2.5
+// document.write(Math.round(a));
+// document.write(Math.floor(a));
+// document.write(Math.ceil(a));
+
+
+// REturn ?
+
+// function multiple(a)
+
+// function add(a = 0, b = 0, c = 0) {
+//     document.write(a + b + c);
+//     document.write("<br>");
+// }
+
+// add(10, 20, 30);
+// add()
+
+// for (let i = 0; i < 5; i++) {
+//     // document.write(i, "hi<br>");
+//     add(i, 10, 20);
+// }
+
+
+// add(20)
+// add(10, 20)
+// add(1, 3, 4, 5, 6);
+
+// object , array
+
+// function add() {
+//     // console.log(arguments);
+//     sum = 0
+//     for (i = 0; i < arguments.length; i++) {
+//         sum += arguments[i];
+//     }
+//     console.log(sum)
+// }
+// ...
+
+// add(10);
+// add(10, 20, 30);
+// add(10, 20, 30, 3, 4, 5, 6);
+
+// return --> 
+
+// function hello(name) {
+//     console.log(name);
+// }
+
+// for (){
+//     hello(i);
+// }
+
+// hello("sachin");
+
+
+// Return ---> exit --> 
+
+// function add(a, b, c) {
+//     let sum = a + b + c;
+//     return sum
+// }
+
+
+// let x1 = add(10, 20, 30)
+// let x2 = add(100, 200, 300)
+// console.log('X1', x1);
+// console.log('X2', x2);
+
+
+
+// *, **
+
+
+// let result = add(10, 20, 30);
+// document.write(result);
+
+// console.log(add(2, 5, 6));
+
+
+// console.log(result / 3);
+
+// var x = 2.55555
+
+// let y = x.toFixed(3) // -->string --> 
+
+// document.write(y);
+
+
+// https://quizizz.com/join?gc=755687
+
+// link for quiz
