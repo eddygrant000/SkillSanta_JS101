8743b8faff61ae4d6212baddc02c19657f5a88a8
Sachin Saini <sachin.saini@ap2v.com>
2021-05-05 19:23:21 +0530

    js array

---
+++ arr.js
@@ -0,0 +1,101 @@
+// Array ---> 
+// let candidates = ["Bhavya", "Isha", "Neel"];
+
+
+// --> 
+// 
+// let name1 = "Abhishek";
+// let name2 = "Hari raj";
+
+// let candidates2 = new Array();
+
+// let candidates1 = [];
+
+// console.log(candidates1);
+
+// ["Bhavya", "Isha", "Neel"]
+//     0        1        2
+// 
+// let candidates = ["Bhavya", "Isha", "Neel"];
+
+// console.log(candidates);
+
+// candidates.push("suraj");
+// candidates.unshift("srujana");
+// console.log(candidates);
+
+
+// console.log(candidates[0]);
+
+
+// candidates.pop();
+// console.log(candidates);
+
+// console.log(candidates[1]);
+// console.log(candidates[2]);
+
+// console.log(candidates.length);
+
+// console.log(candidates.indexOf("Isha", 2));
+// 5 --> "SkillSanta"
+// 5 --> index 4
+// splice(index=4, 0, "SkillSanta")
+
+// let data = [1, 2, 3, 4, "Skillsanta", 6, 7, 8, 9];
+//          0  1  2  3  4  5   6 
+// insert ? --> 
+// data[4] = "JS";
+// console.log(data);
+
+// console.log(data);
+// data.splice(4);
+// console.log(data);
+// let x = data.splice(4);
+// console.log(x);
+// data = data[4];
+// console.log(data);
+// loop ---> 
+// data = data.splice(4);
+// data.splice(1);
+// console.log(data);
+// console.log(data[]);
+// let data = [1, 2, 3, 4, "Skillsanta", 6, 7, 8, 9];
+
+// data.shift()
+// data.shift()
+// data.shift()
+// data.shift()
+
+
+
+
+// for (item in candidates) {
+//     document.write(`${candidates[item]}<br>`);
+//     console.log(item);
+// }
+
+// candidates.push("Sachin")
+// candidates.push("eddy")
+
+// candidates.reverse();
+// let x = candidates.toString()
+
+// console.log(x);
+// 
+//    i = 0   ;  4 < 4 ; i++
+// for (let i = 0; i < candidates.length; i++) {
+//     document.write(`${candidates[i]} <br>`);
+// }
+
+// let candidates = ["Bhavya", "Isha", "Neel", "Aysha"]; // 4
+
+// candidates.forEach(func);
+
+// function func(item) {
+//     console.log(item);
+// }
+
+// func('sachin');
+// func('eddy');
+
+// https://quizizz.com/join?gc=516650
