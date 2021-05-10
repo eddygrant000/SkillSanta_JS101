236dce54ffcab141ba7ccb98ee3ecd70a62b51ce
Sachin Saini <sachin.saini@ap2v.com>
2021-05-10 19:55:42 +0530

    object1

---
+++ today.js
@@ -0,0 +1,88 @@
+// document.write("Hey all!")
+
+
+// Object ---> 
+// String, Number, 
+// Array -> 
+
+// Object --
+
+// let username = "Pappu";
+// let names = [10, 20, 30] // 
+//              0   1   2
+// Object 
+// b
+// u
+// r
+
+// let b1 = {}; //object  literal 
+
+// console.log(typeof(b1));
+
+// let b2 = new Object();
+
+
+// console.log(typeof(b2));
+// new keyword
+// key: value
+
+// let database = {ele1, ele2, ele3}
+
+// let database = {name: "Damini", course: "JS"};
+// methods,  property
+
+// let database = {
+//     name: "Damini",
+//     course: "JS"
+// };
+
+// dot notation : database.name
+// bracket notation : database["course"]
+
+// console.log(database);
+
+// console.log(database.name);
+// console.log(database["course"]);
+// this keyword ? 
+
+// let data = {
+//     name: "Eddy",
+//     age: 22,
+//     addr: "UP12",
+//     rohini: function() {
+//         return `Hey ${this.name} from ${this.addr}`;
+//     }
+// }
+// document.write(data.rohini());
+// document.write("<br>")
+
+// let skillsanta = {
+//     addr: "Delhi",
+//     course: ["JS", "Python", "Ansible"],
+//     details: function() {
+//         console.log(this.course);
+//         return `Hi From ${this.addr} course = ${this.course[1]}`
+//     }
+// }
+
+// document.write(skillsanta.details());
+// class
+// pro
+// methods , about
+
+// let data = {
+//     name: "sachin",
+//     age: 22,
+//     addr: "grurugram"
+// }
+
+// data 
+// data.name2 = "Eddy Grant";
+// delete(data["age"]);
+
+// let i;
+
+// for (i in data) {
+//     document.write(`${i} = ${data[i]} <br>`);
+// }
+// https://quizizz.com/join?gc=195843
