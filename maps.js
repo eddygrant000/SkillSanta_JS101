48864e78d356b8c55d174ff3427df776c3c48d34
Sachin Saini <sachin.saini@ap2v.com>
2021-05-14 19:27:35 +0530

    map in js

---
+++ maps.js
@@ -0,0 +1,82 @@
+// Map --> 
+
+// colleciton key /value 
+// key --> 
+// Object --> you don't 
+// 
+
+
+// console.log(map);
+
+// const map = new Map();
+// map.set('firstName', 'Sachin');
+// map.set('lastName', 'Sachin');
+// map.set('occupation', 'DevOps Engineer');
+
+// map.set('firstName', 'Eddy');
+
+// console.log(map);
+
+// document.write(map.get('firstName'));
+// document.write(map['occupation']);
+// console.log(map.firstName);
+
+// document.write(map.get('occupation'));
+
+
+// let data = [
+//     ['key1', 'value1'],
+//     ['key2', 'value2']
+// ];
+
+// console.log(data);
+// create a map using 
+
+// const map = new Map(data);
+// map.set('org_name', 'SkillSanta');
+// console.log(map);
+// console.log(map.size);
+
+
+// https://quizizz.com/join?gc=246414
+
+// console.log(map);
+
+// let arr = Array.from(map);
+// console.log(arr);
+
+// console.log(map);
+
+// has()
+// console.log(map)
+
+
+// console.log(map.has('address'));
+
+// map.delete('lastName') // true or false
+// map.clear()
+
+// console.log(map);
+
+// const map = new Map();
+// console.log(map);
+
+// map.set('firstName', 'Sachin');
+// map.set('skill', function(a) {
+//     return 'hello world ' + a;
+// });
+
+// console.log(map.get('skill')("Divya"));
+// console.log(map)
+//     // console.log(map.entries());
+
+// // Map.prototype.forEach((value, key, map))
+
+// map.forEach((value, key) => {
+//     console.log(`${key} = ${value}`);
+// })
+// for /of 
+
+// for (const [key, value] of map) {
+//     console.log(`${key} = ${value}`);
+// }
