1736364cbd8b79e18c82b7b3fce6332968a6ae6f
Sachin Saini <sachin.saini@ap2v.com>
2021-05-17 19:22:11 +0530

    set in js

---
+++ sets.js
@@ -0,0 +1,70 @@
+// SET 
+// // 
+// let set1 = new Set();
+
+// set1.add('Terraform'); // add a new element in set
+// set1.add('Hashicorp');
+// set1.add('Teamcity');
+
+// set1.add('Hashicorp');
+
+// let set2 = new Set(['Docker', 'Ansible', "Jenkins", "Docker"]);
+
+// console.log(set2);
+
+// set2.delete('Ansible');
+
+// console.log(set2.has('Ansible')); // return boolean true / false
+
+// console.log(set2.size);
+// set2.clear();
+
+// console.log(set2);
+
+// set1 = Set[10, 20, 30]
+// set2 = Set[100, 200, 20]
+
+// Union    --> [10, 20, 30, 100, 200]
+
+// Intersection --> [20]
+
+
+// Set.prototype.union = function(otherSet) {
+//     var unionSet = new Set();
+
+//     for (var elem of this) {
+//         unionSet.add(elem);
+//     }
+
+//     for (var elem of otherSet) {
+//         unionSet.add(elem);
+//     }
+
+//     return unionSet;
+
+// }
+// var unionset = set1.union(set2);
+
+
+// Set.prototype.intersection = function(otherSet) {
+//     var insersectionSet = new Set();
+
+//     for (var elem of otherSet) {
+//         if (this.has(elem)) {
+//             insersectionSet.add(elem);
+//         }
+//     }
+//     return insersectionSet;
+// }
+
+
+// var set1 = new Set[10, 20, 30, 40, 50]);
+// var set2 = new Set([40, 50, 60, 70, 80]);
+
+// var intersectionset = set1.intersection(set2);
+
+// console.log(intersectionset);
+
+// unionset.forEach((value) => console.log(value));
+
+// https: //quizizz.com/join?gc=700894
